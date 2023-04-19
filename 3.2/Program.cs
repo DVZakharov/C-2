0766a98afd75c9c8126168b950bbe502f903b606
DVZakharov <131305853+DVZakharov@users.noreply.github.com>
2023-04-20 04:33:00 +0700

    Add files via upload

---
+++ 3.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int days;
+            for (int month = 1; month < 13; month++)
+            {
+                bool flag = false;
+                if ((month == 4) || (month == 6) || (month == 9) || (month == 11))
+                {
+                    days = 30;
+                    flag = true;
+                    DateOnly date = new DateOnly(year, month, days);
+                    DayOfWeek day = date.DayOfWeek;
+                    Console.WriteLine($"{ThursdayCheck(day,days)}/{month}/{year} последний четверг месяца"); 
+
+                }
+                if (!flag && month != 2)
+                {
+                    days = 31;
+                    DateOnly date = new DateOnly(year, month, days);
+                    DayOfWeek day = date.DayOfWeek;
+                    Console.WriteLine($"{ThursdayCheck(day,days)}/{month}/{year} последний четверг месяца");
+                }
+                if (month == 2)
+                {
+                    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                        days = 29;
+                    else days = 28;
+                    DateOnly date = new DateOnly(year, month, days);
+                    DayOfWeek day = date.DayOfWeek;
+                    Console.WriteLine($"{ThursdayCheck(day,days)}/{month}/{year} последний четверг месяца");
+                }
+            }
+
+        }
+
+        private static int ThursdayCheck(DayOfWeek day, int d)
+        {
+            switch (day) 
+            {
+                case DayOfWeek.Monday: return d - 4;
+                case DayOfWeek.Tuesday: return d - 5;
+                case DayOfWeek.Wednesday: return d - 6;
+                case DayOfWeek.Thursday: return d;
+                case DayOfWeek.Friday: return d - 1;
+                case DayOfWeek.Saturday: return d - 2;
+                case DayOfWeek.Sunday: return d - 3;
+                default: return d;
+            }
+        }
+    }
+}
---
+++ 3.3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диапазон от 1 до n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите кол-во попыток");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Random rnd = new Random();
+            int rndnum = rnd.Next(1, n+1); 
+            
+            Console.WriteLine("Угадайте число");
+            int attepts = 0;
+            bool win = false;
+            while (attepts < k)
+            {
+                int ps = Convert.ToInt32(Console.ReadLine());
+                if (rndnum == ps)
+                {
+                    Console.WriteLine("Вы угадали");
+                    win = true;
+                    break;
+                }
+                if (rndnum > ps)
+                    Console.WriteLine($"Загаданное число больше {ps}");
+                if (rndnum < ps)
+                    Console.WriteLine($"Загаданное число меньше {ps}");
+                attepts += 1;
+                if (k - attepts == 1) 
+                    Console.WriteLine("Осталась одна попытка");
+            } 
+            if (!win)
+                Console.WriteLine("Вы не угадали");
+        }
+    }
+}
